5378a16f2941e9d75f3df80095a4c42a486936cd
Gabriel Silva <gabriel.silva@mify.com.br>
2026-05-21 09:31:23 -0300

    Atualização do type: commonjs para type: module no package.json e correção dos imports no typescript. Para mais informação: https://stackoverflow.com/questions/61401475/why-is-type-module-in-package-json-file

---
+++ app.ts
@@ -0,0 +1,15 @@
+import { type Response } from 'express';
+import { type Request } from 'express';
+import express from 'express';
+
+
+const app = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
